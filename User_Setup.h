c1e71bd0f88f0c211a5bba85c10be4b7c3ff0bbe
Michal Gerhát <michal.gerhat@gmail.com>
2025-07-08 15:33:21 +0200

    tft_espi setup example

---
+++ User_Setup.h
@@ -0,0 +1,21 @@
+#define USER_SETUP_INFO "User_Setup"
+
+#define GC9A01_DRIVER
+
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 240
+
+#define TFT_MISO  PIN_D6
+#define TFT_MOSI  PIN_D7
+#define TFT_SCLK  PIN_D5
+#define TFT_CS    PIN_D8
+#define TFT_DC    PIN_D3
+#define TFT_RST   PIN_D4
+
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15
+#define TFT_DC   12
+#define TFT_RST   2
+
+#define SPI_FREQUENCY  40000000
